03cc268bd404ade988505b2baef4b52c5d1cbc34
Danielle Knudson <knudson.danielle@gmail.com>
2015-06-22 12:28:26 -0700

    Modify routes view to use User factory and display current route being created

--- public/js/controllers/RoutesController.js
+++ public/js/controllers/RoutesController.js
@@ -1,4 +1,7 @@
-muniButlerApp.controller('RoutesController', function($scope){
-
-
+muniButlerApp.controller('RoutesController', function($scope, User){
+  console.log('in RoutesController')
+  $scope.user = {};
+  $scope.user.username = User.username;
+  $scope.user.firstName = User.firstName;
+  $scope.user.newRouteAddresses = User.newRouteAddresses;
 });
