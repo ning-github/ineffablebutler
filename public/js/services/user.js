bb6e4420d30123e46fd038719c8d0462d1bfcea7
Danielle Knudson <knudson.danielle@gmail.com>
2015-06-23 16:16:35 -0700

    Comment out code to see if user is logged in for loading saved routes

--- public/js/services/user.js
+++ public/js/services/user.js
@@ -1,32 +1,49 @@
 muniButlerApp.factory('User', function(Auth, Routes){
   var user = {};
 
-  if(userlogggin ?) {
-    //  populate user object 
-    user.routes = Routes.get({ id:user.id })
-  } else {
-    user.username = '';
-    user.firstName = '';
-    user.loginMethod = '';
-    user.trip = {
-      from: '178 5th Ave San Francisco, CA 94305',
-      to: '944 Market St San Francisco, CA 94107'
-    };
+  // if(userlogggin ?) {
+  //   //  populate user object 
+  //   user.routes = Routes.get({ id:user.id })
+  // } else {
+  //   user.username = '';
+  //   user.firstName = '';
+  //   user.loginMethod = '';
+  //   user.trip = {
+  //     from: '178 5th Ave San Francisco, CA 94305',
+  //     to: '944 Market St San Francisco, CA 94107'
+  //   };
 
-    // user.routes is an array of objects
-    // each object contains two addresses and two options
-    // {
-    //   to: '944 Market St',
-    //   from: '13333 Candy Lane',
-    //   route: ['38R', 'Geary Blvd & 6th Ave'],
-    // }
-    user.routes = [];
-  }
+  //   // user.routes is an array of objects
+  //   // each object contains two addresses and two options
+  //   // {
+  //   //   to: '944 Market St',
+  //   //   from: '13333 Candy Lane',
+  //   //   route: ['38R', 'Geary Blvd & 6th Ave'],
+  //   // }
+  //   user.routes = [];
+  // }
 
-  console.log(Auth.username)
+  // console.log(Auth.username)
 
   // to save user's routes to database
   Routes.update({id: id:user.id}, user.routes);
+
+  user.username = '';
+  user.firstName = '';
+  user.loginMethod = '';
+  user.trip = {
+    from: '178 5th Ave San Francisco, CA 94305',
+    to: '944 Market St San Francisco, CA 94107'
+  };
+
+  // user.routes is an array of objects
+  // each object contains two addresses and two options
+  // {
+  //   to: '944 Market St',
+  //   from: '13333 Candy Lane',
+  //   route: ['38R', 'Geary Blvd & 6th Ave'],
+  // }
+  user.routes = [];
 
   user.addRoute = function(obj){
     var route = {
