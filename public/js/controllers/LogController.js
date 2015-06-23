c0cce81a8f939cd8709006eec7044f1992acbd37
aytang <albertyptang@gmail.com>
2015-06-23 16:07:34 -0700

    merge

--- public/js/controllers/LogController.js
+++ public/js/controllers/LogController.js
@@ -1,11 +1,11 @@
 muniButlerApp
   .controller('LogController', function ($scope, Auth) {
     $scope.here = false;
-    $scope.options = {};
-    $scope.seekoption = function(){
-      // temporary options, will ask server for actual list
-      $scope.options.facebook = 'facebook';
-      $scope.options.gmail = 'gmail';
-    };
-    Auth.check();
+    // $scope.seekoption = function(){
+    //   // temporary options, will ask server for actual list
+    //   $scope.options.facebook = 'facebook';
+    //   $scope.options.gmail = 'gmail';
+    // };
+    $scope.options = Auth.check();
+    $scope.seekoption = function() {console.log($scope.options);};
   });
