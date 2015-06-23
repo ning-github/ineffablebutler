5839e2e5fe91be793b78deeacb92b62558245ece
aytang <albertyptang@gmail.com>
2015-06-23 16:05:02 -0700

    authorization on home

--- public/js/controllers/LogController.js
+++ public/js/controllers/LogController.js
@@ -1,11 +1,25 @@
 muniButlerApp
   .controller('LogController', function ($scope, Auth) {
     $scope.here = false;
-    // $scope.seekoption = function(){
-    //   // temporary options, will ask server for actual list
-    //   $scope.options.facebook = 'facebook';
-    //   $scope.options.gmail = 'gmail';
-    // };
-    $scope.options = Auth.check();
-    $scope.seekoption = function() {console.log($scope.options);};
+    $scope.loggedin = false;
+    $scope.options = [];
+
+    $scope.logout = function(){ 
+      Auth.logout()
+        .then(function(resp) {
+          return resp.data;
+        }).catch(function(err){
+          console.log(err);
+          return;
+        });
+    };
+
+    Auth.check()
+      .then(function(resp) {
+        $scope.loggedin = true;
+        return resp.data;
+      }).catch(function(err){
+        $scope.options = err.data.authMethods;
+        return;
+      });
   });
