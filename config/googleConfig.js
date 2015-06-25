f5c696b43b8aa65bfb523ba57604bf6ed714aa9e
aytang <albertyptang@gmail.com>
2015-06-25 14:54:27 -0700

    google config for deployment

--- config/googleConfig.js
+++ config/googleConfig.js
@@ -2,6 +2,6 @@ var googleConfig = {
     clientID: '189160676278-ci6rhq98n64ig2ekbb4ojst990u6tg6s.apps.googleusercontent.com',
     clientSecret: 'SkWnFwdTOG69OgDZi092hHqC',
     callbackURL: "http://127.0.0.1:3000/auth/google/callback"
-  }
+  };
 
 module.exports = googleConfig;
