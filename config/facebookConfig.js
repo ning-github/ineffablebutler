40ef20b1cfdf1ace98997ff549e50ab38900b8d7
Eric Kao <ericskao@gmail.com>
2015-06-29 16:01:38 -0700

    Added Facebook Strategy and updated DB models to store new FB user

---
+++ config/facebookConfig.js
@@ -0,0 +1,10 @@
+
+//facebook clientId and Secret
+var facebookConfig = {
+    clientID: '130802563917598',
+    clientSecret: '7a1bb563a6dfebb5c4992bf21470d5ad',
+    callbackURL: "http://127.0.0.1:3000/auth/facebook/callback",
+    enableProof: false
+  };
+
+module.exports = facebookConfig;
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 var UserDB = require('../models/user');
 var googleConfig = require('../config/googleConfig');
+var facebookConfig = require('../config/facebookConfig');
 var authRouter = express.Router();
 
 passport.serializeUser(function (user, done) {
@@ -39,6 +41,28 @@ authRouter.get('/google/callback', passport.authenticate('google', {
   console.log("req user", req.user);
   // Successful authentication, redirect home.
   res.redirect('/');
+});
+
+passport.use(new FacebookStrategy(facebookConfig, function(accessToken, refreshToken, profile, done){
+  console.log("profile is...",profile);
+  UserDB.findOrCreate({
+    facebookId: profile.id,
+    loginMethod: profile.provider,
+    displayName: profile.displayName
+  }, function(err,user,created){
+    if(err) {
+      console.log("error:", err);
+    }
+    console.log("created", created, user, accessToken, refreshToken);
+    return done(null, profile);
+  });
+}));
+
+authRouter.get('/facebook', passport.authenticate('facebook'));
+authRouter.get('/facebook/callback', passport.authenticate('facebook', {failureRedirect: '/'}), 
+  function(req, res){
+    // Successful authentication, redirect home.
+    res.redirect('/');
 });
 
 module.exports = authRouter;
