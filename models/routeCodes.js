ae745f54df592c2e58f80ca629593fd7b5d7d35c
Eric Kao <ericskao@gmail.com>
2015-06-29 12:27:07 -0700

    Comments/refactoring

--- models/routeCodes.js
+++ models/routeCodes.js
@@ -1,9 +1,14 @@
-// Route Stop
-//GetStopsforRoute
+/*
+ ** routeCodes.js
+ ** Schema and model for routes that are fetched from 511xmlfetcher
+ */
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+//routeName is specific route code (ex. 38, 1BX, 3, etc.)
+//routeDir is direction (Inbound or Outbound)
+//routeStop contains stopCodes for each RouteName & routeDirection (ex. 14X Inbound)
 var routeSchema = new Schema({
   routeName: String,
   routeDir: String,
